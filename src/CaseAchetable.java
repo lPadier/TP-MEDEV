122be1aa3a19e64bc9ddc17928e081fdf97ac1b3
misterone <merwanehamadi@gmail.com>
2014-11-10 09:52:03 +0100

    Update CaseAchetable.java

--- src/CaseAchetable.java
+++ src/CaseAchetable.java
@@ -1,22 +1,12 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package monopoly;
 
-/**
- *
- * @author mhamadi
- */
+
 public class CaseAchetable {
     private int prix;
     private Joueur proprietaire;
     
-    public CaseAchetable(String nom, String logo, int position, PlateauJeu plateau, int p, Joueur j){
-        super(nom,logo,position,plateau);
-        this.prix=p;
-        this.proprietaire=j;
-    }
+
     
     public CaseAchetable(int p){
         super(nom,logo,position,plateau);
@@ -33,10 +23,16 @@ public class CaseAchetable {
        return proprietaire;
    }
     
+   public void setPrix(int p){
+       this.prix=p;
+   }
    
-   
+   private void setProprietaire(Joueur j){
+       this.proprietaire=j;
+   }
+  
    public void acheter(Joueur j){
-       //this.proprietaire=j; 
+      
        try {
            
            j.depenser(prix);

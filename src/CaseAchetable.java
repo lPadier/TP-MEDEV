7035f4dd9ae8ba2a4e8f1cac8febe6795f4a8e5b
misterone <merwanehamadi@gmail.com>
2014-11-10 09:47:51 +0100

    Create CaseAchetable.java

---
+++ src/CaseAchetable.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package monopoly;
+
+/**
+ *
+ * @author mhamadi
+ */
+public class CaseAchetable {
+    private int prix;
+    private Joueur proprietaire;
+    
+    public CaseAchetable(String nom, String logo, int position, PlateauJeu plateau, int p, Joueur j){
+        super(nom,logo,position,plateau);
+        this.prix=p;
+        this.proprietaire=j;
+    }
+    
+    public CaseAchetable(int p){
+        this.prix=p;
+        
+    }   
+    
+   public int getPrix(){
+       return prix;
+   }
+   
+   public Joueur getProprietaire(){
+       return proprietaire;
+   }
+    
+   
+   
+   public void acheter(Joueur j){
+       //this.proprietaire=j; 
+       try {
+           
+           j.depenser(prix);
+       }
+       
+       }
+    
+}

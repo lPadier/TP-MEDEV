a9d9c42590a3fa4eecb50b801a46f1a1446580f8
Ophir LOJKINE <pere.jobs@gmail.com>
2014-11-10 09:14:52 +0100

    Création des méthodes

--- src/monopoly/PlateauJeu.java
+++ src/monopoly/PlateauJeu.java
@@ -10,6 +10,7 @@ public class PlateauJeu {
 	private List<Case> cases;
 	
 	public PlateauJeu() {
+		initPlateau();
 	}
 
 	/**
@@ -26,5 +27,36 @@ public class PlateauJeu {
 	 */
 	public int nbCases(Joueur j) {
 		return 0;
+	}
+	
+	/**
+	 * Affiche le plateau
+	 */
+	public void affiche() {
+		
+	}
+	
+	/**
+	 * Fait tourner le jeu pour un tour
+	 */
+	public void tourDeJeu(){
+		
+	}
+	
+
+	/**
+	 * @param c La case de départ
+	 * @param nbCases
+	 * @return La case qui est nbCases après c sur le plateau
+	 */
+	public Case avance(Case c, int nbCases) {
+		return c;
+	}
+	
+	/**
+	 * Initialise le plateau
+	 */
+	public void initPlateau() {
+		
 	}
 }

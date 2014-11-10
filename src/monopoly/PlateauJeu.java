d2d86785ed7fa5e1ec5402565cb1ab0659c9a31a
Ophir LOJKINE <pere.jobs@gmail.com>
2014-11-10 09:05:32 +0100

    Premières méthodes du plateau de jeu

--- src/monopoly/PlateauJeu.java
+++ src/monopoly/PlateauJeu.java
@@ -12,4 +12,19 @@ public class PlateauJeu {
 	public PlateauJeu() {
 	}
 
+	/**
+	 * @return true si la partie est terminée
+	 */
+	public boolean finDePartie(){
+		return false;
+	}
+	
+	
+	/**
+	 * @param j le joueur
+	 * @return le nombre de cases possédées par le joueur j
+	 */
+	public int nbCases(Joueur j) {
+		return 0;
+	}
 }

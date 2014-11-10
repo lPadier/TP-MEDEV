c73b9021ccde8ed5e9a887447f0381a4821c5d49
Ophir LOJKINE <pere.jobs@gmail.com>
2014-11-10 08:57:31 +0100

    Création des classes de base

---
+++ src/monopoly/PlateauJeu.java
@@ -0,0 +1,15 @@
+package monopoly;
+
+import java.util.List;
+
+import monopoly.cases.Case;
+
+public class PlateauJeu {
+
+	private List<Joueur> joueurs;
+	private List<Case> cases;
+	
+	public PlateauJeu() {
+	}
+
+}
---
+++ src/monopoly/cases/Case.java
@@ -0,0 +1,5 @@
+package monopoly.cases;
+
+public class Case {
+
+}
